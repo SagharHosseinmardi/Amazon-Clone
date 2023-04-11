de6784b4d0668c564ec3734f8da084c4467249dd
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-04-11 15:43:38 +0100

    passed products's info as props

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({id, title, price, rating, image}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>Title</p>
-        <p className="product__price">$30</p>
-        <div className="product__rating">⭐️⭐️⭐️</div>
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating).fill().map((_, i)=>(<p>⭐️</p>))}
+        </div>
       </div>
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpghttps://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61XUZ6gvDQL._AC_SY879_.jpg"
+        src={image}
         alt="product_image"
       />
 
